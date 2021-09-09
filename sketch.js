6e42330025bf51e12da0b656b0f35a18da675be4
Elemed-Hai <33613079+Elemed-Hai@users.noreply.github.com>
2021-09-09 01:38:15 -0500

    Added Cells

--- sketch.js
+++ sketch.js
@@ -11,6 +11,32 @@ let sketch = function(p) {
     p.setup = function(){
       p.createCanvas(canvasWidth, canvasHeight);
       p.background(0);
+
+      for(let i = 0; i < gridSize; i++)
+      {
+        grid[i] = new Array(gridSize);
+        for(let j = 0; j < gridSize; j++)
+        {
+          grid[i][j] = new Cell(i, j, sizePerCell);
+          if(random() > 0.5) grid[i][j].isOpen = true;
+        }
+      }
+
+      p.fill(0, 160, 160);
+      p.noStroke();
+    }
+
+    p.draw = function() {
+      p.background(0);
+
+      for(let i = 0; i < gridSize; i++)
+      {
+        for(let j = 0; j < gridSize; j++)
+        {
+          grid[i][j].DrawSelf();
+        }
+      }
+      
     }
   };
 new p5(sketch, 'exampleContainer');
